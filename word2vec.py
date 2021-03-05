62975fdd84e53ec11e76203c82165420942b7e4d
akshatgoel92 <akshatgoel92@hotmail.com>
2021-03-05 12:01:45 +0530

    Initial commit

---
+++ word2vec.py
@@ -0,0 +1,36 @@
+import numpy as np 
+
+
+
+class Word2Vec():
+
+
+    def __init__():
+        pass
+
+
+
+    def forward():
+        pass
+
+
+    def backward():
+        pass 
+
+
+    def train():
+        pass 
+
+
+
+    def test():
+        pass 
+
+
+
+
+
+
+if __name__ == '__main__': 
+
+    print("Hello world...!")
